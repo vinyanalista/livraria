dc436df1a55e5dfb5ec31b66dd3d3baf1ba0058a
Antônio Vinícius <vinyanalista@gmail.com>
2014-01-19 17:10:10 -0300

    Configuração própria para a máquina virtual

--- implantar.bat
+++ implantar.bat
@@ -7,6 +7,7 @@ echo.
 call mvn clean compile package install
 call mvn glassfish:undeploy
 call mvn glassfish:deploy
+call mvn glassfish:redeploy
 echo.
 echo A aplicacao ja pode ser acessada pelo endereco http://localhost:8080/livraria/
 pause
