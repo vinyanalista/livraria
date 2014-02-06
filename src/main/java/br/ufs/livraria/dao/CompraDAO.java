6de9b21e942b6f24d62253525b73ae9b176c94d6
livraria <adrianodiasx93@gmail.com>
2014-02-06 20:53:24 -0300

    Adicionado CompraDAO

---
+++ src/main/java/br/ufs/livraria/dao/CompraDAO.java
@@ -0,0 +1,40 @@
+package br.ufs.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+
+import br.ufs.livraria.modelo.Compra;
+import br.ufs.livraria.modelo.Pagamento;
+import br.ufs.livraria.modelo.Usuario;
+
+
+@Stateless
+public class CompraDAO extends DAO<Compra> {
+	private static final long serialVersionUID = 1L;
+	
+	public CompraDAO() {
+		super(Compra.class);
+	}
+	
+	
+	
+	public List<Compra> listar(Usuario usuario) {
+		return entityManager.createQuery(
+				"SELECT entidade FROM " + Compra.class.getSimpleName()
+						+ " entidade"+
+						"WHERE cliente_id = "+usuario.getId(), Compra.class).getResultList();
+	}
+	
+	//Compras com Pagamentos Aprovados
+	public List<Compra> aprovados() {
+		return entityManager.createQuery(
+				"SELECT entidade "+
+		        "FROM " + Compra.class.getSimpleName()+" entidade "+
+			    "JOIN "+Pagamento.class.getSimpleName()+" pagamento"+
+				"ON entidade.pagamento_id = pagamento.id"+
+				"WHERE pagamento.aprovado = 1", Compra.class).getResultList();
+	}
+	
+	
+}
